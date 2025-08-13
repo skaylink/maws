e6cec0e3fde8115322672dc2c8654ff60f7accbd
Jochen Werner <jochen.werner@skaylink.com>
2025-08-13 11:06:29 +0200

    Update sources

--- app/commands/ecs.py
+++ app/commands/ecs.py
@@ -1,16 +1,73 @@
+from http import HTTPStatus
+from typing import Callable
+
 import typer
-from pydantic import BaseModel, HttpUrl
 from rich.console import Console
+
+from app.clients.api import HTTPException
+from app.clients.api.models import ECSServiceDeploymentRequest
+from app.clients.api.models.V1ServiceResonseStatus import V1ServiceResonseStatus
+from app.clients.api.services.DeploymentV1_service import get_service, post_deployment
+from app.config import env
 
 app = typer.Typer(no_args_is_help=True)
 console = Console()
 
 
-class schema(BaseModel):
-    foo: HttpUrl
+@app.command()
+def deploy(
+    service_name: str = typer.Argument(..., help="Name of the ECS service to deploy"),
+) -> None:
+    """Create an ECS service deployment"""
+
+    def start() -> None:
+        post_deployment(data=ECSServiceDeploymentRequest(service=service_name), api_config_override=env.api_config)
+        console.print(f"Deployment successfully started for service [italic]{service_name}[/italic]", style="cyan")
+        status(service_name)
+
+    if not typer.confirm(f"Deploy service '{service_name}'?"):
+        console.print("Deployment cancelled", style="yellow")
+        return
+
+    return _withExceptionHandler(fn=start)
 
 
 @app.command()
-def deploy() -> None:
+def status(
+    service_name: str = typer.Argument(..., help="Name of the ECS service to check"),
+) -> None:
+    """Get the status of an ECS service deployment"""
 
-    console.print("Deployment started", overflow="fold", style="bold green")
+    def check(delay: int = 5) -> None:
+        import time
+
+        while True:
+            response = get_service(service_name, env.api_config)
+            if HTTPStatus.OK == response.status_code:
+                loop_status = [V1ServiceResonseStatus("PENDING").value, V1ServiceResonseStatus("IN_PROGRESS").value]
+                data = response.json()
+                if data.get("status") in loop_status:
+                    console.print(".", style="cyan")
+                else:
+                    console.print("Deployment successfully", style="bold green")
+                    break
+            else:
+                raise HTTPException(status_code=response.status_code, message=response.json())
+            time.sleep(delay)
+
+    console.print(f"Checking status for service {service_name}..", style="cyan")
+
+    return _withExceptionHandler(fn=check)
+
+
+def _withExceptionHandler(fn: Callable) -> None:
+    try:
+        fn()
+    except HTTPException as e:
+        console.print("API Error", style="bold red")
+        console.print(e, overflow="fold")
+        return typer.Abort()
+    except Exception as e:
+        console.print("Unexpected error", style="red")
+        console.print(e, overflow="fold", style="bold red")
+        return typer.Abort()
--- app/config.py
+++ app/config.py
@@ -2,12 +2,14 @@ import os
 
 from pydantic_settings import BaseSettings
 
+from app.clients.api import APIConfig
+
 from . import __app_name__, __version__
 
 
 class DotEnvSettings(BaseSettings):
-    endpoint: str = os.getenv("ENDPOINT")
-    secret: str = os.getenv("SECRET")
+    api_base_url: str = os.getenv("API_BASE_URL")
+    access_token: str = os.getenv("API_ACCESS_TOKEN")
 
 
 class Settings(DotEnvSettings):
@@ -15,6 +17,16 @@ class Settings(DotEnvSettings):
         "name": __app_name__,
         "version": __version__,
     }
+
+    @property
+    def api_config(cls) -> APIConfig:
+        """
+        Define the API configuration with base URL and optional access token.
+
+        Returns:
+            APIConfig
+        """
+        return APIConfig(base_path=cls.api_base_url, access_token=cls.access_token)
 
 
 def get_settings():
