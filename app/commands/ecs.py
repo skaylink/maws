0056a8b635c1aa4084f77c9d93498913a3274d37
Jochen Werner <109971619+skaylink-jwe@users.noreply.github.com>
2025-08-20 14:46:15 +0200

    AWHPF-961: ECS Deployment command adjusted (#8)

--- app/commands/ecs.py
+++ app/commands/ecs.py
@@ -1,19 +1,14 @@
-import json
 import time
 from http import HTTPStatus
 
 import typer
-from munch import DefaultMunch
 from rich.console import Console
 
 from app.clients.ecs_service_deployment_client.api.deployment import (
     get_service,
     post_deployment,
 )
-from app.clients.ecs_service_deployment_client.models import (
-    ServiceDeploymentRequest,
-    ServiceResponseStatus,
-)
+from app.clients.ecs_service_deployment_client.models import ServiceDeploymentRequest
 from app.config import env
 
 app = typer.Typer(no_args_is_help=True)
@@ -21,23 +16,32 @@ console = Console()
 
 
 @app.command()
-def deploy(service_name: str = typer.Argument(..., help="Name of the ECS service to deploy")) -> None:
-    """Create an ECS service deployment"""
+def deploy(
+    service_name: str = typer.Argument(help="The name of the service to be updated"),
+    image: str = typer.Argument(help="The container image to use for the service"),
+    force: bool = typer.Option(False, help="Force new deployment, event if images has not changed"),
+) -> None:
+    """
+
+    ECS Service Deployment Request
+
+    Args:
+        service_name (str)
+        image (str)
+        force (str, Optional):  Defaults to False.
+
+    Raises:
+        Exception
+    """
     try:
         response = post_deployment.sync_detailed(
-            client=env.api_client, body=ServiceDeploymentRequest(service_name=service_name)
+            client=env.api_client, body=ServiceDeploymentRequest(service_name=service_name, image=image, force=force)
         )
-        parsed = DefaultMunch.fromDict(json.loads(response.content))
         if response.status_code == HTTPStatus.CREATED:
-            if parsed.status == ServiceResponseStatus.PENDING:
-                console.print(
-                    f"Deployment successfully started for service [italic]{service_name}[/italic]", style="cyan"
-                )
-                return status(service_name)
-            else:
-                console.print(parsed.status, style="yellow")
+            console.print(f"Deployment successfully started for service [italic]{service_name}[/italic]", style="green")
+            return status(service_name=service_name, delay=5)
         else:
-            console.print(parsed.errors, style="red")
+            console.print(f"[ERROR] {response.parsed.error}", style="red")
             raise Exception(f"Deployment failed with status {response.status_code}")
     except Exception as e:
         console.print(e, overflow="fold", style="red")
@@ -46,26 +50,34 @@ def deploy(service_name: str = typer.Argument(..., help="Name of the ECS service
 
 @app.command()
 def status(
-    service_name: str = typer.Argument(..., help="Name of the ECS service to check"),
+    service_name: str = typer.Argument(help="Name of the ECS service to check"),
     delay: int = typer.Option(5, help="The delay to check if the ECS service status"),
 ) -> None:
-    """Get the status of an ECS service deployment"""
-    console.print(f"Checking status for service [italic]{service_name}[/italic]", end="", style="cyan")
+    """
+    Get the status of an ECS service deployment
+
+    Args:
+        service_name (str)
+        delay (int, optional):  Defaults to 5.
+
+    Raises:
+        Exception
+    """
+    console.print(f"Checking deployment status for service [italic]{service_name}[/italic]", end="", style="cyan")
     try:
         while True:
             response = get_service.sync_detailed(service=service_name, client=env.api_client)
-            if response.status_code == HTTPStatus.OK:
-                if response.parsed.status in [ServiceResponseStatus.PENDING, ServiceResponseStatus.IN_PROGRESS]:
+            match response.status_code:
+                case HTTPStatus.ACCEPTED:
                     console.print(".", end="", style="cyan")
-                else:
-                    console.print(": DONE", style="cyan")
-                    console.print(
-                        f"Deployment finished with status {response.parsed.status}",
-                        style="green" if response.parsed.status == ServiceResponseStatus.SUCCESSFUL else "red",
-                    )
+                case HTTPStatus.EXPECTATION_FAILED:
+                    console.print(f"\nDeployment failed with status {response.status_code}.", style="red")
                     break
-            else:
-                raise Exception(f"Status check failed with status {response.status_code}")
+                case HTTPStatus.OK:
+                    console.print(f"\nDeployment succeeded with status {response.status_code}.", style="green")
+                    break
+                case _:
+                    raise Exception(f"Deployment failed with status {response.status_code}.")
             time.sleep(delay)
     except Exception as e:
         console.print(e, overflow="fold", style="red")
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,39 +1,46 @@
 import json
+from http import HTTPStatus
 from unittest.mock import Mock
 
 import pytest
 from faker import Faker
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def fake():
     return Faker()
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def mock_env_vars(fake):
     return {
         "API_BASE_URL": fake.url(),
-        "API_VERSION": fake.word(),
+        "API_VERSION": "v1",
         "API_ACCESS_TOKEN": fake.uuid4(),
     }
 
 
-@pytest.fixture
-def mock_authenticated_client():
+@pytest.fixture(scope="function")
+def mock_response_failed(fake) -> callable:
+    def make(error: str = fake.sentence()) -> Mock:
+        response = Mock()
+        response.status_code = HTTPStatus.EXPECTATION_FAILED
+        response.content = json.dumps({"error": error})
+        return response
+
+    yield make
+
+
+@pytest.fixture(scope="function")
+def mock_post_deployment():
     return Mock()
 
 
-@pytest.fixture
-def mock_response(fake):
-    response = Mock()
-    response.status_code = 200
-    response.content = json.dumps({"status": "SUCCESSFUL", "message": fake.sentence()})
-    response.parsed = Mock()
-    response.parsed.status = "SUCCESSFUL"
-    return response
-
-
-@pytest.fixture
+@pytest.fixture(scope="function")
 def mock_console():
+    return Mock()
+
+
+@pytest.fixture(scope="function")
+def mock_authenticated_client():
     return Mock()
