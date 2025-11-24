242bf163349f7b8f79b8b4acb5adf3b5c99da29d
Stefan Heitmüller <stefan.heitmueller@skaylink.com>
2025-11-24 14:28:41 +0100

    add secret-arns
    Signed-off-by: Stefan Heitmüller <stefan.heitmueller@skaylink.com>

--- src/rubber_duck/commands/ecs.py
+++ src/rubber_duck/commands/ecs.py
@@ -1,6 +1,7 @@
 import json
 import time
 from http import HTTPStatus
+from re import match
 
 import typer
 from rich.console import Console
@@ -18,11 +19,27 @@ app = typer.Typer(no_args_is_help=True)
 console = Console()
 
 
+def secret_arn_pattern(value):
+    pattern = r"arn:aws:secretsmanager:[a-z0-9-]+:[0-9]+:secret:.*"
+    for item in value:
+        if not match(pattern, item):
+            raise typer.BadParameter(f"Does not match the pattern: {pattern}")
+    return value
+
+
 @app.command()
 def deploy(
     service_name: str = typer.Argument(help="The name of the service to be updated"),
     image: str = typer.Argument(help="The container image to use for the service"),
-    force: bool = typer.Option(False, help="Force new deployment, event if images has not changed"),
+    force: bool = typer.Option(
+        False,
+        help="Force new deployment, event if images has not changed",
+    ),
+    secret_arns: list[str] = typer.Option(
+        [],
+        help="List of secret ARNs to attach to the service",
+        callback=secret_arn_pattern,
+    ),
     profile: str = typer.Option(help=f"Profile name from {str(CONFIG_FILE_PATH)}"),
 ) -> None:
     """
@@ -32,6 +49,7 @@ def deploy(
         service_name (str)
         image (str)
         force (str, Optional):  Defaults to False.
+        secret_arns (list[str], Optional)
         profile (str, Optional): Profile name
 
     Raises:
@@ -40,10 +58,15 @@ def deploy(
     env = get_settings(profile)
     try:
         response = patch_service.sync_detailed(
-            service=service_name, client=env.api_client, body=ServiceDeploymentRequest(image=image, force=force)
+            service=service_name,
+            client=env.api_client,
+            body=ServiceDeploymentRequest(image=image, force=force, secret_arns=secret_arns),
         )
         if response.status_code == HTTPStatus.CREATED:
-            console.print(f"Deployment successfully started for service [italic]{service_name}[/italic]", style="green")
+            console.print(
+                f"Deployment successfully started for service [italic]{service_name}[/italic]",
+                style="green",
+            )
             return status(service_name=service_name, delay=5, profile=profile)
         else:
             content = json.loads(response.content)
@@ -72,7 +95,11 @@ def status(
         Exception
     """
     env = get_settings(profile)
-    console.print(f"Checking deployment status for service [italic]{service_name}[/italic]", end="", style="cyan")
+    console.print(
+        f"Checking deployment status for service [italic]{service_name}[/italic]",
+        end="",
+        style="cyan",
+    )
     try:
         while True:
             response = get_service.sync_detailed(service=service_name, client=env.api_client)
@@ -80,10 +107,16 @@ def status(
                 case HTTPStatus.ACCEPTED:
                     console.print(".", end="", style="cyan")
                 case HTTPStatus.EXPECTATION_FAILED:
-                    console.print(f"\nDeployment failed with status {response.status_code}.", style="red")
+                    console.print(
+                        f"\nDeployment failed with status {response.status_code}.",
+                        style="red",
+                    )
                     break
                 case HTTPStatus.OK:
-                    console.print(f"\nDeployment succeeded with status {response.status_code}.", style="green")
+                    console.print(
+                        f"\nDeployment succeeded with status {response.status_code}.",
+                        style="green",
+                    )
                     break
                 case _:
                     raise Exception(f"\nDeployment failed with status {response.status_code}.")
